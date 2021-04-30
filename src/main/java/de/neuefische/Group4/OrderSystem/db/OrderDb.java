cd4da62792b53a931740e4609df3e829f66c2b86
Fabian F <h-f.fromme@posteo.de>
2021-04-30 13:00:32 +0200

    Add addOrder in OrderDb

--- src/main/java/de/neuefische/Group4/OrderSystem/db/OrderDb.java
+++ src/main/java/de/neuefische/Group4/OrderSystem/db/OrderDb.java
@@ -1,7 +1,17 @@
 package de.neuefische.Group4.OrderSystem.db;
 
-public class OrderDb {
-    add(){
+import de.neuefische.Group4.OrderSystem.model.Order;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDb {
+    //Variables
+    List<Order> ordersList = new ArrayList<Order>();
+
+    //Methods
+    public void addOrder(String[] products){
+        String id = String.valueOf(ordersList.size());
+        ordersList.add(new Order(id, products));
     }
 }
