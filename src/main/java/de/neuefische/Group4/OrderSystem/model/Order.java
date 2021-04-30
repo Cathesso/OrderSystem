67aacc5235a9802d7858711cd65952a5d4c20b24
Fabian F <h-f.fromme@posteo.de>
2021-04-30 12:40:11 +0200

    Add Getter & Setter & Has & Equals for Order & Product

--- src/main/java/de/neuefische/Group4/OrderSystem/model/Order.java
+++ src/main/java/de/neuefische/Group4/OrderSystem/model/Order.java
@@ -1,4 +1,46 @@
 package de.neuefische.Group4.OrderSystem.model;
 
+import java.util.Objects;
+
 public class Order {
+    //Variables
+    private String orderId;
+    private String[] products;
+
+    //Constructor
+    public Order(String orderId, String[] products) {
+        this.orderId = orderId;
+        this.products = products;
+    }
+
+    //Getter & Setter
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(String[] products) {
+        this.products = products;
+    }
+
+    //Hash & Equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return orderId.equals(order.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId);
+    }
 }
--- src/main/java/de/neuefische/Group4/OrderSystem/model/Product.java
+++ src/main/java/de/neuefische/Group4/OrderSystem/model/Product.java
@@ -1,7 +1,50 @@
 package de.neuefische.Group4.OrderSystem.model;
 
-public class Product {
-    add(){
+import java.util.Objects;
 
+public class Product {
+    //Variables
+    private String productId;
+    private String name;
+
+    //Constructor
+    public Product(String productId, String name) {
+        this.productId = productId;
+        this.name = name;
     }
+
+    //Methods
+
+
+    //Getter & Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    //Hash & Equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return name.equals(product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
 }
