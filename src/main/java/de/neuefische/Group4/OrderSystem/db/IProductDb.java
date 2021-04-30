36ac8e1347a7151e8fc835ef112530d90d3b516c
Fabian F <h-f.fromme@posteo.de>
2021-04-30 11:33:26 +0200

    Implement Interfaces

--- src/main/java/de/neuefische/Group4/OrderSystem/db/IProductDb.java
+++ src/main/java/de/neuefische/Group4/OrderSystem/db/IProductDb.java
@@ -1,4 +1,6 @@
 package de.neuefische.Group4.OrderSystem.db;
 
 public interface IProductDb {
+    public Product[] list(); //Übergabewerte: Entweder String oder Product
+    public Product get(String id); //
 }
--- src/main/java/de/neuefische/Group4/OrderSystem/db/ProductDb.java
+++ src/main/java/de/neuefische/Group4/OrderSystem/db/ProductDb.java
@@ -1,4 +1,13 @@
 package de.neuefische.Group4.OrderSystem.db;
 
-public class ProductDb {
+public class ProductDb implements IProductDb{
+    @Override
+    public Product[] list(int zahl) {
+        return new Product[0];
+    }
+
+    @Override
+    public Product get() {
+        return null;
+    }
 }
